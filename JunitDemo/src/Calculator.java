c059b671af9ebf8efe5137042e384faed99df3e5
jainshivani1996 <47006594+jainshivani1996@users.noreply.github.com>
2020-03-11 15:45:22 +0530

    Update Calculator.java

--- JunitDemo/src/Calculator.java
+++ JunitDemo/src/Calculator.java
@@ -18,8 +18,13 @@ public class Calculator {
 		 return a/b;
 		 
 	 }
-	public static void main(String args[]){
+	
+	
+}
+class Calc
+{
+public static void main(String args[]){
 Calculator c=new Calculator();
-		c.add(10,20);
-	}
+		System.out.println(c.add(10,20));
+}
 }
